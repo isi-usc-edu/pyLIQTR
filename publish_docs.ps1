149d30688306caf3e1cbbea382651dac80708fc2
rrood <robert.rood@ll.mit.edu>
2024-12-20 14:01:44 -0500

    Deploy docs to GitHub Pages

--- publish_docs.ps1
+++ publish_docs.ps1
@@ -9,5 +9,5 @@ git config --local user.name 'rrood'
 git config --local --add safe.directory '*'
 git add .
 git commit -m "Deploy docs to GitHub Pages"
-git remote add origin https://github.com/isi-usc-edu/pyLIQTR/quantum_algorithms.git
+git remote add origin https://github.com/isi-usc-edu/pyLIQTR.git
 git push --force --set-upstream origin master:gh-pages
